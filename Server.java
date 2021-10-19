c14253fc2a90c50bb43900615f4659accac6e2ff
chaowen guo <chaowen.guo1@gmail.com>
2021-10-19 00:19:30 -0500

    Create Server.java

---
+++ Server.java
@@ -0,0 +1,17 @@
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@SpringBootApplication
+public class Server {
+
+	@RequestMapping("/")
+	public String index(){
+		return "Hello Spring Boot";
+	}
+	public static void main(String[] args) {
+		SpringApplication.run(Chapter1Application.class, args);
+	}
+}
