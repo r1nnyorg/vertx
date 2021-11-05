e6fe2c51a3615697092e86731788e71bfe7fc26b
chaowen guo <chaowen.guo1@gmail.com>
2021-11-05 07:49:41 -0500

    Update Server.java

--- Server.java
+++ Server.java
@@ -4,7 +4,7 @@ public class Server
     {
 	final var route = org.springframework.web.reactive.function.server.RouterFunctions.route(org.springframework.web.reactive.function.server.RequestPredicates.GET("/"), request -> org.springframework.web.reactive.function.server.ServerResponse.ok().body(org.springframework.web.reactive.function.BodyInserters.fromObject("Hello")));
         final var httpHandler = org.springframework.web.reactive.function.server.RouterFunctions.toHttpHandler(route);
-	final var adapter = new org.springframework.http.server.reactive.ReactorHttpHandlerAdapter(handler);
+	final var adapter = new org.springframework.http.server.reactive.ReactorHttpHandlerAdapter(httpHandler);
         reactor.netty.http.server.HttpServer.create().port(80).handle(adapter).bind().block();
     }
 }
