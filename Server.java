fbe39a08e37032d75cd9b7fe62eb1fe3351e82a4
chaowen guo <chaowen.guo1@gmail.com>
2021-11-05 11:14:02 -0500

    Update Server.java

--- Server.java
+++ Server.java
@@ -1,7 +1,10 @@
 public class Server
 {
-    public static void main(final String[] args)
+    public static void main(final java.lang.String[] args)
     {
+        final var connectionFactory = new io.r2dbc.postgresql.PostgresqlConnectionFactory(io.r2dbc.postgresql.PostgresqlConnectionConfiguration.builder().host("postgrespostgres.postgres.database.azure.com").username("postgres").password("pos1gres+").database("default").build());
+	final var client = org.springframework.r2dbc.core.DatabaseClient.create(connectionFactory);
+	System.out.println(client.sql('select * from productItem').fetch().all().block());
 	final var route = org.springframework.web.reactive.function.server.RouterFunctions.route(org.springframework.web.reactive.function.server.RequestPredicates.GET("/"), request -> org.springframework.web.reactive.function.server.ServerResponse.ok().body(org.springframework.web.reactive.function.BodyInserters.fromValue("Hello")));
         final var httpHandler = org.springframework.web.reactive.function.server.RouterFunctions.toHttpHandler(route);
 	final var adapter = new org.springframework.http.server.reactive.ReactorHttpHandlerAdapter(httpHandler);
