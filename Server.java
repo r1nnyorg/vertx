cd9e6f8a572e91bdaaeda8e349d554179ca9d936
chaowen guo <chaowen.guo1@gmail.com>
2022-02-21 02:17:04 -0600

    Update Server.java

--- Server.java
+++ Server.java
@@ -4,10 +4,9 @@ public class Server
     {
         final var connectionFactory = new io.r2dbc.postgresql.PostgresqlConnectionFactory(io.r2dbc.postgresql.PostgresqlConnectionConfiguration.builder().host("postgrespostgres.postgres.database.azure.com").username("postgres").password("pos1gres+").database("default").enableSsl().build());
 	final var client = org.springframework.r2dbc.core.DatabaseClient.create(connectionFactory);
-	final var records = /*new com.fasterxml.jackson.databind.ObjectMapper().writeValueAsString(*/ client.sql("select * from productItem").fetch().all(); //.toStream().collect(java.util.stream.Collectors.toList()));
 	final var route = org.springframework.web.reactive.function.server.RouterFunctions.route(org.springframework.web.reactive.function.server.RequestPredicates.GET("/"), request ->
         {
-	    return org.springframework.web.reactive.function.server.ServerResponse.ok().body(records, java.util.Map.class);
+	    return org.springframework.web.reactive.function.server.ServerResponse.ok().body(client.sql("select * from productItem").fetch().all(), java.util.Map.class);
 	});
         final var httpHandler = org.springframework.web.reactive.function.server.RouterFunctions.toHttpHandler(route);
 	final var adapter = new org.springframework.http.server.reactive.ReactorHttpHandlerAdapter(httpHandler);
