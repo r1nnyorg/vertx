81bc23baf4b38e6370a582623d2e4152479968a5
chaowen guo <chaowen.guo1@gmail.com>
2022-02-23 00:19:31 -0600

    Update Server.java

--- Server.java
+++ Server.java
@@ -5,18 +5,18 @@ public class Server
         final var connectionFactory = new io.r2dbc.postgresql.PostgresqlConnectionFactory(io.r2dbc.postgresql.PostgresqlConnectionConfiguration.builder().host("postgrespostgres.postgres.database.azure.com").username("postgres").password("pos1gres+").database("default").enableSsl().build());
 	final var client = org.springframework.r2dbc.core.DatabaseClient.create(connectionFactory);
 	final var objectMapper = new com.fasterxml.jackson.databind.ObjectMapper();
-	final var route = org.springframework.web.reactive.function.server.RouterFunctions.route(org.springframework.web.reactive.function.server.RequestPredicates.POST("/ajax"), request ->
+	final var route = org.springframework.web.reactive.function.server.RouterFunctions.route(org.springframework.web.reactive.function.server.RequestPredicates.POST("/ajax"), request -> org.springframework.web.reactive.function.server.ServerResponse.ok().body(request.bodyToFlux(java.lang.String.class).map(body ->
 {
     try
     {
-        return org.springframework.web.reactive.function.server.ServerResponse.ok().body(request.bodyToFlux(java.lang.String.class).map(body -> objectMapper.readValue(body, new com.fasterxml.jackson.core.type.TypeReference<java.util.Map<java.lang.String, java.lang.String>>(){}).get("limit")), java.lang.String.class);
+        return objectMapper.readValue(body, new com.fasterxml.jackson.core.type.TypeReference<java.util.Map<java.lang.String, java.lang.String>>(){}).get("limit"));
         //return org.springframework.web.reactive.function.server.ServerResponse.ok().body(request.bodyToFlux(java.lang.String.class).flatMap(body -> client.sql(objectMapper.readValue(body, new com.fasterxml.jackson.core.type.TypeReference<java.util.Map<java.lang.String, java.lang.String>>(){}).entrySet().stream().map(entry -> java.lang.String.join(" ", entry.getKey(), entry.getValue())).collect(java.util.stream.Collectors.joining(" "))).fetch().all()), java.util.Map.class);
     }
     catch (final java.lang.Exception e)
     {
-	return null;
+	return "";
     }
-});
+}), java.lang.String.class));
         final var httpHandler = org.springframework.web.reactive.function.server.RouterFunctions.toHttpHandler(route);
 	final var adapter = new org.springframework.http.server.reactive.ReactorHttpHandlerAdapter(httpHandler);
         final var server = reactor.netty.http.server.HttpServer.create().port(80).handle(adapter).bindNow();
