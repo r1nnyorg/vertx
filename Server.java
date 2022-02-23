f26cbfc7853cfd28055e8059e9da67c4debc7a17
chaowen guo <chaowen.guo1@gmail.com>
2022-02-22 23:33:52 -0600

    Update Server.java

--- Server.java
+++ Server.java
@@ -9,8 +9,7 @@ public class Server
 {
     try
     {
-        return org.springframework.web.reactive.function.server.ServerResponse.ok().body(request.bodyToFlux(java.lang.String.class).flatMap(body -> client.sql(objectMapper.readValue(body, new com.fasterxml.jackson.core.type.TypeReference<java.util.Map<java.lang.String, java.lang.String>>(){}).entrySet().stream().map(entry -> java.lang.String.join(" ", entry.getKey(), entry.getValue())).collect(java.util.stream.Collectors
-.joining(" "))).fetch().all()), java.util.Map.class);
+        return org.springframework.web.reactive.function.server.ServerResponse.ok().body(request.bodyToFlux(java.lang.String.class).flatMap(body -> client.sql(objectMapper.readValue(body, new com.fasterxml.jackson.core.type.TypeReference<java.util.Map<java.lang.String, java.lang.String>>(){}).entrySet().stream().map(entry -> java.lang.String.join(" ", entry.getKey(), entry.getValue())).collect(java.util.stream.Collectors.joining(" "))).fetch().all()), java.util.Map.class);
     }
     catch (final java.lang.Exception e){}
 });
