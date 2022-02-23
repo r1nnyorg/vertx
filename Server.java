40e6a5aaad6ab06afbc3a07d76609ce562c66239
chaowen guo <chaowen.guo1@gmail.com>
2022-02-22 21:44:13 -0600

    Update Server.java

--- Server.java
+++ Server.java
@@ -1,10 +1,10 @@
 public class Server
 {
-    public static void main(final java.lang.String[] args) throws Exception
+    public static void main(final java.lang.String[] args)
     {
         final var connectionFactory = new io.r2dbc.postgresql.PostgresqlConnectionFactory(io.r2dbc.postgresql.PostgresqlConnectionConfiguration.builder().host("postgrespostgres.postgres.database.azure.com").username("postgres").password("pos1gres+").database("default").enableSsl().build());
 	final var client = org.springframework.r2dbc.core.DatabaseClient.create(connectionFactory);
-	final var route = org.springframework.web.reactive.function.server.RouterFunctions.route(org.springframework.web.reactive.function.server.RequestPredicates.GET("/"), request -> org.springframework.web.reactive.function.server.ServerResponse.ok().body(client.sql("select * from productItem").fetch().all(), java.util.Map.class));
+	final var route = org.springframework.web.reactive.function.server.RouterFunctions.route(org.springframework.web.reactive.function.server.RequestPredicates.GET("/"), request -> request.bodyToMono(java.lang.String.class).map(body -> org.springframework.web.reactive.function.server.ServerResponse.ok().body(client.sql(body).fetch().all(), java.util.Map.class)));
         final var httpHandler = org.springframework.web.reactive.function.server.RouterFunctions.toHttpHandler(route);
 	final var adapter = new org.springframework.http.server.reactive.ReactorHttpHandlerAdapter(httpHandler);
         final var server = reactor.netty.http.server.HttpServer.create().port(80).handle(adapter).bindNow();
