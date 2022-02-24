63db63fac0117ba321104b9a41cc880d1daf4afe
chaowen guo <chaowen.guo1@gmail.com>
2022-02-24 01:08:27 -0600

    Update Server.java

--- Server.java
+++ Server.java
@@ -5,7 +5,7 @@ public class Server
         final var vertx = io.vertx.core.Vertx.vertx();
         final var router = io.vertx.ext.web.Router.router(vertx);
         router.route().handler(io.vertx.ext.web.handler.CorsHandler.create()).handler(io.vertx.ext.web.handler.BodyHandler.create());
-        final var database = io.vertx.pgclient.PgPool.pool(vertx, new io.vertx.pgclient.PgConnectOptions().setHost("postgrespostgres.postgres.database.azure.com").setDatabase("default").setUser("postgres").setPassword("pos1gres+"), new io.vertx.sqlclient.PoolOptions());
+        final var database = io.vertx.pgclient.PgPool.pool(vertx, new io.vertx.pgclient.PgConnectOptions().setHost("postgrespostgres.postgres.database.azure.com").setDatabase("default").setUser("postgres").setPassword("pos1gres+").setSsl(true), new io.vertx.sqlclient.PoolOptions());
         router.post("/ajax").respond(ctx -> database.query("select * from" + ctx.getBodyAsJson().stream().map($ -> java.lang.String.join(" ", $.getKey(), $.getValue().toString())).collect(java.util.stream.Collectors.joining(" "))).execute()   );  
         vertx.createHttpServer().requestHandler(router).listen(80);
     }
