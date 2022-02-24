b2d5e3e20c31d4abd72ef0b937aaf6987f28e2fd
chaowen guo <chaowen.guo1@gmail.com>
2022-02-23 23:42:33 -0600

    Update Server.java

--- Server.java
+++ Server.java
@@ -2,6 +2,10 @@ public class Server
 {
     public static void main(final java.lang.String[] args)
     {
-        io.vertx.core.Vertx.vertx().createHttpServer().requestHandler(request -> request.response().end("fuck")).listen(80);
+        final var vertx = io.vertx.core.Vertx.vertx();
+        final var router = io.vertx.ext.web.Router.router(vertx);
+        router.route().handler(io.vertx.ext.web.handler.CorsHandler.create()).handler(io.vertx.ext.web.handler.BodyHandler.create());
+        router.post("/ajax").respond(ctx -> ctx.response().end(ctx.getBodyAsJson().toString()));  
+        vertx.createHttpServer().requestHandler(router).listen(80);
     }
 }
