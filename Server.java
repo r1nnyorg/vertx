bc7a3ea824591ed1d6bf68696b86c93c1785700b
chaowen guo <chaowen.guo1@gmail.com>
2022-02-24 00:07:27 -0600

    Update Server.java

--- Server.java
+++ Server.java
@@ -5,7 +5,7 @@ public class Server
         final var vertx = io.vertx.core.Vertx.vertx();
         final var router = io.vertx.ext.web.Router.router(vertx);
         router.route().handler(io.vertx.ext.web.handler.CorsHandler.create()).handler(io.vertx.ext.web.handler.BodyHandler.create());
-        router.post("/ajax").respond(ctx -> io.vertx.core.Future.succeededFuture(ctx.getBodyAsJson()));  
+        router.post("/ajax").respond(ctx -> io.vertx.core.Future.succeededFuture(ctx.getBodyAsJson().stream().map($ -> java.lang.String.join(" ", $.getKey(), $.getValue().toString())).collect(java.util.stream.Collectors.joining(" "))));  
         vertx.createHttpServer().requestHandler(router).listen(80);
     }
 }
