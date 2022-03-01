9ca4ef80d244482764c20f4f3628209aa47681cb
chaowen guo <chaowen.guo1@gmail.com>
2022-03-01 03:05:34 -0600

    Update Server.java

--- Server.java
+++ Server.java
@@ -2,6 +2,8 @@ public class Server
 {
     public static void main(final java.lang.String[] args)
     {
+        final var address = java.net.InetAddress.getByName("postgrespostgres.postgres.database.azure.com"); 
+        System.out.println(address.getHostAddress()); 
         final var vertx = io.vertx.core.Vertx.vertx();
         final var router = io.vertx.ext.web.Router.router(vertx);
         router.route().handler(io.vertx.ext.web.handler.CorsHandler.create()).handler(io.vertx.ext.web.handler.BodyHandler.create());
