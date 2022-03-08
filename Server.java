c5d9c452e73f15418f2be76222497f6d7b5f5c7b
chaowen guo <chaowen.guo1@gmail.com>
2022-03-08 01:21:19 -0600

    Update Server.java

--- Server.java
+++ Server.java
@@ -6,8 +6,14 @@ public class Server
         final var vertx = io.vertx.core.Vertx.vertx();
         final var router = io.vertx.ext.web.Router.router(vertx);
         router.route().handler(io.vertx.ext.web.handler.CorsHandler.create()).handler(io.vertx.ext.web.handler.BodyHandler.create());
+        final var client = io.vertx.redis.client.Redis.createClient(vertx, "redis://redis");
+        final var redis = io.vertx.redis.client.RedisAPI.api(client);
         final var database = io.vertx.pgclient.PgPool.pool(vertx, new io.vertx.pgclient.PgConnectOptions().setHost("postgrespostgres.postgres.database.azure.com").setDatabase("default").setUser("postgres").setPassword("pos1gres+").setSsl(true).setPemTrustOptions(new io.vertx.core.net.PemTrustOptions().addCertPath("DigiCertGlobalRootCA.crt.pem")), new io.vertx.sqlclient.PoolOptions());
-        router.post("/ajax").respond(ctx -> database.query("select * from" + ctx.getBodyAsJson().stream().map($ -> java.lang.String.join(" ", $.getKey(), $.getValue().toString())).collect(java.util.stream.Collectors.joining(" "))).mapping($ -> $.toJson()).execute()       );  
+        router.post("/ajax").respond(ctx ->
+{
+    final var body = ctx.getBodyAsJson().stream().map($ -> java.lang.String.join(" ", $.getKey(), $.getValue().toString())).collect(java.util.stream.Collectors.joining(" "));
+    client.connect().compose(connection -> redis.get(body)).compose(records -> java.util.Objects.isNull(records) ? database.query("select * from" + body).mapping($ -> $.toJson()).execute() : records);
+});  
         vertx.createHttpServer().requestHandler(router).listen(80);
     }
 }
