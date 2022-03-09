519cf2a4ecf2fd8da642e8cb224cd2ac72421f54
chaowen guo <chaowen.guo1@gmail.com>
2022-03-09 01:27:45 -0600

    Update ip6.sh

--- ip6.sh
+++ ip6.sh
@@ -1,7 +1,7 @@
 apt update
 apt install -y --no-install-recommends docker.io
 echo 'nameserver 2a03:7900:2:0:31:3:104:161' > /etc/resolv.conf
-echo {\"ipv6\":true, \"fixed-cidr-v6\":\"fd00::/64\"} > /etc/docker/daemon.json
+echo '{"ipv6":true, "fixed-cidr-v6":"fd00::/64"}' > /etc/docker/daemon.json
 ip6tables -t nat -I POSTROUTING -j MASQUERADE
 systemctl restart docker
 mkdir -p /etc/letsencrypt/live/chaowenguo.eu.org
