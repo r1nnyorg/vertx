08d58b74cc2d07430403bcc99412c4899371382e
chaowen guo <chaowen.guo1@gmail.com>
2022-03-22 03:22:12 -0500

    Update Server.java

--- Server.java
+++ Server.java
@@ -6,10 +6,10 @@ public class Server
         final var vertx = io.vertx.core.Vertx.vertx();
         final var router = io.vertx.ext.web.Router.router(vertx);
         router.route().handler(io.vertx.ext.web.handler.CorsHandler.create()).handler(io.vertx.ext.web.handler.BodyHandler.create());
-        //final var client = io.vertx.redis.client.Redis.createClient(vertx, "redis://redis");
-        final var client = io.vertx.redis.client.Redis.createClient(vertx, new io.vertx.redis.client.RedisOptions().setType(io.vertx.redis.client.RedisClientType.CLUSTER).addConnectionString("redis://redis").setPassword(java.lang.System.getenv("password")));
+        final var client = io.vertx.redis.client.Redis.createClient(vertx, new io.vertx.redis.client.RedisOptions().setType(io.vertx.redis.client.RedisClientType.CLUSTER).addConnectionString("redis://redis").setPassword(java.lang.System.getenv("password"))); //final var client = io.vertx.redis.client.Redis.createClient(vertx, "redis://redis");
         final var redis = io.vertx.redis.client.RedisAPI.api(client);
-        final var database = io.vertx.pgclient.PgPool.pool(vertx, new io.vertx.pgclient.PgConnectOptions().setHost("postgrespostgres.postgres.database.azure.com").setDatabase("default").setUser("postgres").setPassword("pos1gres+").setSsl(true).setPemTrustOptions(new io.vertx.core.net.PemTrustOptions().addCertPath("DigiCertGlobalRootCA.crt.pem")), new io.vertx.sqlclient.PoolOptions());
+        //final var database = io.vertx.pgclient.PgPool.pool(vertx, new io.vertx.pgclient.PgConnectOptions().setHost("postgrespostgres.postgres.database.azure.com").setDatabase("default").setUser("postgres").setPassword("pos1gres+").setSsl(true).setPemTrustOptions(new io.vertx.core.net.PemTrustOptions().addCertPath("DigiCertGlobalRootCA.crt.pem")), new io.vertx.sqlclient.PoolOptions());
+        final var database = io.vertx.pgclient.PgPool.pool(vertx, new io.vertx.pgclient.PgConnectOptions().setHost("cockroach").setDatabase("defaultdb").setUser("root").setSsl(true), new io.vertx.sqlclient.PoolOptions());
         router.post("/ajax").respond(ctx ->
 {
     final var body = ctx.getBodyAsJson().stream().map($ -> java.lang.String.join(" ", $.getKey(), $.getValue().toString())).collect(java.util.stream.Collectors.joining(" "));
